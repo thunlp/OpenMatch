e8a2256253e3d13dfb66d53072cbe51998bb26f2
zkt12 <347925985@qq.com>
2020-06-21 02:35:47 +0800

    fix kernel matcher

--- OpenMatch/modules/matchers/kernel_matcher.py
+++ OpenMatch/modules/matchers/kernel_matcher.py
@@ -13,8 +13,8 @@ class KernelMatcher(nn.Module):
         self._embed_dim = embed_dim
         self._kernel_num = kernel_num
         mus, sigmas = self.kernel_init(self._kernel_num)
-        self._mus = nn.Parameter(mus)
-        self._sigmas = nn.Parameter(sigmas)
+        self._mus = nn.Parameter(mus, requires_grad=False)
+        self._sigmas = nn.Parameter(sigmas, requires_grad=False)
 
     def kernel_init(self, kernel_num: int) -> Dict[str, torch.Tensor]:
         mus = [1]
@@ -22,23 +22,22 @@ class KernelMatcher(nn.Module):
         mus.append(1-bin_size/2)
         for i in range(1, kernel_num-1):
             mus.append(mus[i]-bin_size)
-        mus = torch.tensor(mus, requires_grad=False).view(1, 1, 1, kernel_num)
+        mus = torch.tensor(mus).view(1, 1, 1, kernel_num)
 
         sigmas = [0.001]
         sigmas += [0.1]*(kernel_num-1)
-        sigmas = torch.tensor(sigmas, requires_grad=False).view(1, 1, 1, kernel_num)
+        sigmas = torch.tensor(sigmas).view(1, 1, 1, kernel_num)
         return mus, sigmas
 
     def forward(self, k_embed: torch.Tensor, k_mask: torch.Tensor, v_embed: torch.Tensor, v_mask: torch.Tensor) -> torch.Tensor:
         k_embed = k_embed * k_mask.unsqueeze(-1)
         v_embed = v_embed * v_mask.unsqueeze(-1)
         k_by_v_mask = torch.bmm(k_mask.float().unsqueeze(-1), v_mask.float().unsqueeze(-1).transpose(1, 2))
-        #k_norm = k_embed / (k_embed.norm(p=2, dim=-1, keepdim=True) + 1e-13)
-        #v_norm = v_embed / (v_embed.norm(p=2, dim=-1, keepdim=True) + 1e-13)
+        k_norm = k_embed / (k_embed.norm(p=2, dim=-1, keepdim=True) + 1e-10)
+        v_norm = v_embed / (v_embed.norm(p=2, dim=-1, keepdim=True) + 1e-10)
         inter = (torch.bmm(k_norm, v_norm.transpose(1, 2)) * k_by_v_mask).unsqueeze(-1)
 
         kernel_outputs = torch.exp((-((inter-self._mus)**2)/(self._sigmas**2)/2))
-        #kernel_outputs = kernel_outputs.sum(dim=2).clamp(min=1e-10).log() * 1e-2
-        kernel_outputs = (kernel_outputs.sum(dim=2) + 1e-6).log()
+        kernel_outputs = kernel_outputs.sum(dim=2).clamp(min=1e-10).log() * 1e-2
         logits = kernel_outputs.sum(dim=1)
         return logits
