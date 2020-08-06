76bf0a0ed37d8fcf7086582669f819e01297afac
zkt12 <347925985@qq.com>
2020-08-06 16:23:38 +0800

    update script

--- train_bert.sh
+++ train_bert.sh
@@ -4,16 +4,16 @@ python train.py \
         -model bert \
         -train ./data/train_toy.jsonl \
         -max_input 1280000 \
-        -save ./checkpoints/scibert.bin \
+        -save ./checkpoints/bert.bin \
         -dev ./data/dev_toy.jsonl \
         -qrels ./data/qrels_toy \
-        -vocab allenai/scibert_scivocab_uncased \
-        -pretrain allenai/scibert_scivocab_uncased \
-        -res ./results/scibert.trec \
+        -vocab bert-base-uncased \
+        -pretrain bert-base-uncased \
+        -res ./results/bert.trec \
         -metric ndcg_cut_10 \
         -max_query_len 32 \
         -max_doc_len 256 \
         -epoch 1 \
         -batch_size 4 \
         -lr 2e-5 \
-        -eval_every 1000
+        -eval_every 10
