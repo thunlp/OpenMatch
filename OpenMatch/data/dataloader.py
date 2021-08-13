0b1f733385df30534570f9a9c155d49c76ff5b3f
David Jin <daaaaavjd@gmail.com>
2021-08-12 18:49:00 -0700

    Update dataloader.py
    distributed function fix

--- OpenMatch/data/dataloader.py
+++ OpenMatch/data/dataloader.py
@@ -1,7 +1,8 @@
 from typing import List, Dict, Any
 
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Sampler
+from torch.utils.data.distributed import DistributedSampler
 
 from OpenMatch.data.datasets import Dataset
 
@@ -11,12 +12,14 @@ class DataLoader(DataLoader):
         dataset: Dataset,
         batch_size: int,
         shuffle: str = False,
-        num_workers: int = 0
+        num_workers: int = 0,
+        sampler = None,
     ) -> None:
         super().__init__(
             dataset = dataset,
             batch_size = batch_size,
             shuffle = shuffle,
             num_workers = num_workers,
-            collate_fn = dataset.collate
+            collate_fn = dataset.collate,
+            sampler = sampler,
         )
