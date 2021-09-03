e0a774d5df446450661a79293f7e6fd6a8d1bbbd
GregxmHu <89115107+GregxmHu@users.noreply.github.com>
2021-09-03 15:22:18 +0800

    add t5Dataset

--- OpenMatch/data/datasets/__init__.py
+++ OpenMatch/data/datasets/__init__.py
@@ -4,3 +4,4 @@ from OpenMatch.data.datasets.bert_dataset import BertDataset
 from OpenMatch.data.datasets.roberta_dataset import RobertaDataset
 from OpenMatch.data.datasets.bertmlm_dataset import BertMLMDataset
 from OpenMatch.data.datasets.bertmaxp_dataset import BertMaxPDataset
+from OpenMatch.data.datasets.t5_dataset import t5Dataset
