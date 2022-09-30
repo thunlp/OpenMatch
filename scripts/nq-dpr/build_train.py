c9454111ffab4ef383da8834301f0176425ecf2b
Shi Yu <yushi17@foxmail.com>
2022-09-30 09:17:48 +0000

    multiprocessing tokenization on NQ

--- scripts/nq-dpr/build_train.py
+++ scripts/nq-dpr/build_train.py
@@ -3,18 +3,23 @@
 import json
 import os
 from argparse import ArgumentParser
+from multiprocessing import Pool
 
-from transformers import AutoTokenizer, PreTrainedTokenizer
-from tqdm import tqdm
 from openmatch.utils import fill_template
+from tqdm import tqdm
+from transformers import AutoTokenizer, PreTrainedTokenizer
 
 parser = ArgumentParser()
 parser.add_argument('--input', type=str, required=True)
 parser.add_argument('--output', type=str, required=True)
-parser.add_argument('--query_template', type=str)
-parser.add_argument('--doc_template', type=str)
+parser.add_argument('--query_template', type=str, default="<question>")
+parser.add_argument('--doc_template', type=str, default="<title> [SEP] <text>")
+parser.add_argument('--query_max_len', type=int, default=32)
+parser.add_argument('--doc_max_len', type=int, default=256)
 parser.add_argument('--tokenizer', type=str, required=False, default='bert-base-uncased')
-parser.add_argument('--minimum-negatives', type=int, required=False, default=1)
+parser.add_argument('--minimum_negatives', type=int, required=False, default=1)
+parser.add_argument('--mp_chunk_size', type=int, default=500)
+parser.add_argument('--num_workers', type=int, default=4)
 args = parser.parse_args()
 
 tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(args.tokenizer, use_fast=True)
@@ -25,27 +30,38 @@ save_dir = os.path.split(args.output)[0]
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
+doc_markers = ["title", "text"]
+
+
+def process_item(item):
+    if len(item['hard_negative_ctxs']) < args.minimum_negatives or len(item['positive_ctxs']) < 1:
+        return
+    group = {}
+    positives = []
+    for pos in item['positive_ctxs']:
+        positives.append(fill_template(args.doc_template, pos, doc_markers))
+    negatives = []
+    for neg in item['hard_negative_ctxs']:
+        negatives.append(fill_template(args.doc_template, neg, doc_markers))
+
+    query = tokenizer.encode(fill_template(args.query_template, item, ["question"]), add_special_tokens=False, max_length=args.query_max_len, truncation=True)
+    positives = tokenizer(
+            positives, add_special_tokens=False, max_length=args.doc_max_len, truncation=True, padding=False)['input_ids']
+    negatives = tokenizer(
+            negatives, add_special_tokens=False, max_length=args.doc_max_len, truncation=True, padding=False)['input_ids']
+
+    group['query'] = query
+    group['positives'] = positives
+    group['negatives'] = negatives
+
+    return group
+
+
+with Pool(args.num_workers) as p:
+    groups = list(tqdm(p.imap(process_item, data, chunksize=args.mp_chunk_size), total=len(data)))
+
 with open(args.output, 'w') as f:
-    for idx, item in enumerate(tqdm(data)):
-        if len(item['hard_negative_ctxs']) < args.minimum_negatives or len(item['positive_ctxs']) < 1:
+    for group in tqdm(groups):
+        if group is None:
             continue
-
-        group = {}
-        positives = []
-        for pos in item['positive_ctxs']:
-            positives.append(fill_template(args.doc_template, pos))
-        negatives = []
-        for neg in item['hard_negative_ctxs']:
-            negatives.append(fill_template(args.doc_template, neg))
-
-        query = tokenizer.encode(fill_template(args.query_template, item), add_special_tokens=False, max_length=32, truncation=True)
-        positives = tokenizer(
-            positives, add_special_tokens=False, max_length=128, truncation=True, padding=False)['input_ids']
-        negatives = tokenizer(
-            negatives, add_special_tokens=False, max_length=128, truncation=True, padding=False)['input_ids']
-
-        group['query'] = query
-        group['positives'] = positives
-        group['negatives'] = negatives
-
-        f.write(json.dumps(group) + '\n')
+        f.write(json.dumps(group) + "\n")
